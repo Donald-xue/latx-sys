62411144a814d00b5c86bf3b07557fac7115aa92
Hanlu Li <lihanlu@loongson.cn>
2021-05-17 21:11:54 +0800

    linux-user: Add ioctl(SIOCETHTOOL, ...) support
    Signed-off-by: Hanlu Li <lihanlu@loongson.cn>

--- linux-user/syscall_defs.h
+++ linux-user/syscall_defs.h
@@ -906,6 +906,7 @@ struct target_rtc_pll_info {
 
 #define TARGET_SIOCGIFTXQLEN   0x8942          /* Get the tx queue length      */
 #define TARGET_SIOCSIFTXQLEN   0x8943          /* Set the tx queue length      */
+#define TARGET_SIOCETHTOOL     0x8946          /* Ethtool interface            */
 
 /* ARP cache control calls. */
 #define TARGET_OLD_SIOCDARP    0x8950          /* old delete ARP table entry   */
