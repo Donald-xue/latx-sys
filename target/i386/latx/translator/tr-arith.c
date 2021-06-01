7c1ed6061409ea4363bbbeede55de90c3c40963e
Jinyang Shen <shenjinyang@loongson.cn>
2021-06-01 16:34:00 +0800

    LATX: fix translate_xadd
    Add processing when the reg opnd is AL/AH in translate_xadd.
    DBT test is passed and WPS can run without fault.
    
    Signed-off-by: Jinyang Shen <shenjinyang@loongson.cn>

--- target/i386/latx/translator/tr-arith.c
+++ target/i386/latx/translator/tr-arith.c
@@ -595,48 +595,131 @@ bool translate_xadd(IR1_INST *pir1)
 {
     IR1_OPND *opnd0 = ir1_get_opnd(pir1, 0);
     IR1_OPND *opnd1 = ir1_get_opnd(pir1, 1);
-    IR2_OPND src_opnd_1 =
+    IR2_OPND src_reg_opnd =
         load_ireg_from_ir1(opnd1, SIGN_EXTENSION, false);
     IR2_OPND dest_opnd = ra_alloc_itemp();
 
     if (ir1_is_prefix_lock(pir1)) {
-        if (ir1_opnd_size(ir1_get_opnd(pir1, 0)) != 32) {
-            lsassertm(0, "Invalid operand size (%d) in %s.\n",
-                      ir1_opnd_size(ir1_get_opnd(pir1, 0)), __func__);
+        /* opnd0 is mem, opnd1 is reg */
+        IR1_OPND *reg_ir1 = NULL;
+        if (ir1_opnd_size(opnd0) == 16) {
+            lsassertm(0, "Invalid operand size (%d) in translate_xadd.\n",
+                        ir1_opnd_size(opnd0));
         }
+
+        /* make sure which reg is in the pir1, al/ah/ax ? */
+        if (ir1_opnd_size(ir1_get_opnd(pir1, 1)) == 32)
+            reg_ir1 = &eax_ir1_opnd;
+        else if (ir1_opnd_base_reg_bits_start(ir1_get_opnd(pir1, 1)))
+            reg_ir1 = &ah_ir1_opnd;
+        else
+            reg_ir1 = &al_ir1_opnd;
 
         IR2_OPND label_ll = ir2_opnd_new_type(IR2_OPND_LABEL);
         /* mem addr */
+        IR2_OPND tmp_opnd = ra_alloc_itemp();
+        IR2_OPND imm_opnd = ra_alloc_itemp();
         IR2_OPND sc_opnd = ra_alloc_itemp();
-        IR2_OPND src_opnd_0 = ra_alloc_itemp();
+        IR2_OPND mask_opnd = ra_alloc_itemp();
+        IR2_OPND src_mem_opnd = ra_alloc_itemp();
+        IR2_OPND src_mem_opnd_cpy = ra_alloc_itemp();
+        IR2_OPND src_mem_opnd_restore = ra_alloc_itemp();
+        IR2_OPND mem_opnd_cpy = ra_alloc_itemp();
+        IR2_OPND dest_opnd_cpy = ra_alloc_itemp();
+
         IR2_OPND mem_opnd = mem_ir1_to_ir2_opnd(opnd0, false);
         int imm = ir2_opnd_imm(&mem_opnd);
         mem_opnd._type = IR2_OPND_IREG;
 
+        /* 1. make a copy of mem_opnd before we do anything,
+         *    because next inst may use it in the same tb */
+        la_append_ir2_opnd3_em(LISA_OR, mem_opnd_cpy, zero_ir2_opnd, mem_opnd);
+
+        /* 2. add the offset to the base memory adderss */
+        load_ireg_from_imm32(imm_opnd, imm, SIGN_EXTENSION);
+        la_append_ir2_opnd3_em(LISA_ADD_D, mem_opnd_cpy, mem_opnd_cpy, imm_opnd);
+
         la_append_ir2_opnd1(LISA_LABEL, label_ll);
-        la_append_ir2_opnd2i_em(LISA_LL_W, src_opnd_0, mem_opnd, imm);
-        la_append_ir2_opnd3_em(LISA_ADD_W, dest_opnd, src_opnd_0, src_opnd_1);
+
+        /* 3. (original-address % 4) => the target-byte's offset in the 4-bytes loaded*/
+        load_ireg_from_imm32(imm_opnd, 0x4, ZERO_EXTENSION);
+        la_append_ir2_opnd3(LISA_MOD_WU, tmp_opnd, mem_opnd_cpy, imm_opnd);
+        la_append_ir2_opnd3_em(LISA_SUB_D, mem_opnd_cpy, mem_opnd_cpy, tmp_opnd);
+
+        /* 4. now ll the total 4 bytes to reg and make a copy of src_mem_opnd */
+        la_append_ir2_opnd2i_em(LISA_LL_W, src_mem_opnd, mem_opnd_cpy, 0);
+        la_append_ir2_opnd3_em(LISA_OR, src_mem_opnd_restore, zero_ir2_opnd, src_mem_opnd);
+
+        if (ir1_opnd_size(opnd0) == 8) {
+            /* 5. rebuild src_mem_opnd */
+            /* 5.1. make a copy of src_mem_opnd */
+            la_append_ir2_opnd3_em(LISA_OR, src_mem_opnd_cpy, zero_ir2_opnd, src_mem_opnd);
+            /* 5.2. extract the single byte in src_mem_opnd_cpy which is specified by tmp_opnd,
+             *      and shift it right to the lowest 8 bit. */
+            la_append_ir2_opnd2i_em(LISA_SLLI_D, tmp_opnd, tmp_opnd, 3);
+            la_append_ir2_opnd3_em(LISA_SRL_D, src_mem_opnd_cpy, src_mem_opnd_cpy, tmp_opnd);
+            la_append_ir2_opnd2ii(LISA_BSTRINS_D, src_mem_opnd_cpy, zero_ir2_opnd, 31, 8);
+            /* 5.3. clear the single byte in src_mem_opnd which is specified tmp_opnd */
+            load_ireg_from_imm32(mask_opnd, 0xff, ZERO_EXTENSION);
+            la_append_ir2_opnd3_em(LISA_SLL_D, mask_opnd, mask_opnd, tmp_opnd);
+            la_append_ir2_opnd3_em(LISA_NOR, mask_opnd, zero_ir2_opnd, mask_opnd);
+            la_append_ir2_opnd3_em(LISA_AND, src_mem_opnd, src_mem_opnd, mask_opnd);
+            /* 5.4. do the ADD operation, so we get pre-target dest_opnd whose
+             *      data is in the lowest 8-bit */
+            la_append_ir2_opnd2ii(LISA_BSTRINS_D, src_reg_opnd, zero_ir2_opnd, 31, 8);
+            la_append_ir2_opnd3_em(LISA_ADD_W, dest_opnd, src_mem_opnd_cpy, src_reg_opnd);
+            /* this LISA_BSTRINS_D is to solve the problem of overflow, the dest should always
+             * has 8 bits. */
+            la_append_ir2_opnd2ii(LISA_BSTRINS_D, dest_opnd, zero_ir2_opnd, 31, 8);
+            la_append_ir2_opnd3_em(LISA_OR, dest_opnd_cpy, zero_ir2_opnd, dest_opnd);
+            /* 5.5. shift it left to the coresponding positon specified by tmp_opnd */
+            la_append_ir2_opnd3_em(LISA_SLL_D, dest_opnd, dest_opnd, tmp_opnd);
+            /* 5.6. do the OR operation, finally we get the final-target dest_opnd */
+            la_append_ir2_opnd3_em(LISA_OR, dest_opnd, dest_opnd, src_mem_opnd);
+        } else if (ir1_opnd_size(opnd0) == 16) {
+            lsassertm(0, "Invalid operand size (%d) in translate_xadd.\n",
+                        ir1_opnd_size(opnd0));
+        } else {
+            /* 32-bit go through without any additional processing */
+            la_append_ir2_opnd3_em(LISA_ADD_W, dest_opnd, src_mem_opnd, src_reg_opnd);
+        }
+
+        //la_append_ir2_opnd3_em(LISA_ADD_W, dest_opnd, src_mem_opnd, src_reg_opnd);
         la_append_ir2_opnd3_em(LISA_OR, sc_opnd, zero_ir2_opnd, dest_opnd);
-        la_append_ir2_opnd2i(LISA_SC_W, sc_opnd, mem_opnd, imm);
+        la_append_ir2_opnd2i(LISA_SC_W, sc_opnd, mem_opnd_cpy, 0);
+        la_append_ir2_opnd2i_em(LISA_SRLI_D, tmp_opnd, tmp_opnd, 3);
+        la_append_ir2_opnd3_em(LISA_ADD_D, mem_opnd_cpy, mem_opnd_cpy, tmp_opnd);
         la_append_ir2_opnd3(LISA_BEQ, sc_opnd, zero_ir2_opnd, label_ll);
 
-        generate_eflag_calculation(dest_opnd, src_opnd_0, src_opnd_1, pir1, true);
-        store_ireg_to_ir1(src_opnd_0, opnd1, false);
+        if (ir1_opnd_size(opnd0) == 8) {
+            /* there is a little trouble in eflags stuff: we need to guarantee the two opnds which are
+             * used to calculate eflags are the original opnds, or eflag will generated unexpected,
+             * so we use src_mem_opnd_cpy but not src_mem_opnd here, you can check the generate_eflag_calculation
+             * if you are confused. */
+            generate_eflag_calculation(dest_opnd_cpy, src_reg_opnd, src_mem_opnd_cpy, pir1, true);
+            store_ireg_to_ir1(src_mem_opnd_restore, reg_ir1, false);
+        } else if (ir1_opnd_size(opnd0) == 16) {
+            lsassertm(0, "Invalid operand size (%d) in translate_xadd.\n",
+                        ir1_opnd_size(opnd0));
+        } else {
+            generate_eflag_calculation(dest_opnd, src_reg_opnd, src_mem_opnd, pir1, true);
+            store_ireg_to_ir1(src_mem_opnd_restore, opnd1, false);
+        }
         ra_free_temp(sc_opnd);
-        ra_free_temp(src_opnd_0);
+        ra_free_temp(src_mem_opnd);
     } else {
-        IR2_OPND src_opnd_0 =
+        IR2_OPND src_mem_opnd =
             load_ireg_from_ir1(opnd0, SIGN_EXTENSION, false);
-        la_append_ir2_opnd3_em(LISA_ADD_W, dest_opnd, src_opnd_0, src_opnd_1);
-        generate_eflag_calculation(dest_opnd, src_opnd_0, src_opnd_1, pir1, true);
+        la_append_ir2_opnd3_em(LISA_ADD_W, dest_opnd, src_mem_opnd, src_reg_opnd);
+        generate_eflag_calculation(dest_opnd, src_mem_opnd, src_reg_opnd, pir1, true);
 
         if (ir1_opnd_is_gpr(opnd0)) {
-            store_ireg_to_ir1(src_opnd_0, opnd1, false);
+            store_ireg_to_ir1(src_mem_opnd, opnd1, false);
             store_ireg_to_ir1(dest_opnd, opnd0, false);
         } else {
             lsassert(ir1_opnd_is_mem(opnd0));
             store_ireg_to_ir1(dest_opnd, opnd0, false);
-            store_ireg_to_ir1(src_opnd_0, opnd1, false);
+            store_ireg_to_ir1(src_mem_opnd, opnd1, false);
         }
     }
 
