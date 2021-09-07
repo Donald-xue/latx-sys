d2c7d3da7f3d96131d6cb8522bb2c3cadcfc30c6
NiuGenen <niugen@loongson.cn>
2021-09-07 14:25:25 +0800

    LATX-SYS: fix sigint do unlink and relink for indirect jmp link
    Reuse tb->jmp_reset_offset[0] and tb->jmp_target_arg[0] to unlink TB.
    
    Add one flag TB.is_indir_tb to info wether this TB ends with an indirect jmp TB-Link.
    
    Easy to relink: set jump to native_jmp_glue_2
    
    Change-Id: I9dce60a43985f2a5b95660308cd1f2b992a5f43d
    Signed-off-by: NiuGenen <niugen@loongson.cn>

--- target/i386/latx/translator/sys-eob.c
+++ target/i386/latx/translator/sys-eob.c
@@ -665,12 +665,16 @@ indirect_jmp:
 
             ADDR code_buf = (ADDR)tb->tc.ptr;
             int offset = td->real_ir2_inst_num << 2;
+
+            IR2_OPND indirect_jmp_glue_label = latxs_ir2_opnd_new_label();
+            latxs_append_ir2_opnd1(LISA_LABEL, &indirect_jmp_glue_label);
             latxs_append_ir2_opnda(LISA_B,
                     (native_jmp_glue_2 - code_buf - offset) >> 2);
-
-        } else {
-            latxs_tr_gen_exit_tb_j_context_switch(NULL, 0, succ_id);
+            tb->jmp_reset_offset[0] = indirect_jmp_glue_label.val;
+            latxs_append_ir2_opnd0_(lisa_nop);
+            tb->is_indir_tb = 1;
         }
+        latxs_tr_gen_exit_tb_j_context_switch(NULL, 0, succ_id);
         break;
     case X86_INS_JE:
     case X86_INS_JNE:
