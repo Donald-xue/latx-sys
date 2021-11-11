Donald-xue/latx-sys

commit f515e57339ac899eed53bf070d547331b2c70c43

LATX-SYS: fix latx sigint vcpu will not signal it-self
In tcg_handle_interrupt(), when cpu thread is self, we do NOT need to send signal since we are already
executing in vCPU thread. So we could just do the unlink job.

-----------------------------------------------------------------------------------------------------

Change-Id: Ie8afc24ae75b03624fb674b8df344a283e8d4d1a
Signed-off-by: NiuGenen <niugen@loongson.cn>

accel/tcg/tcg-accel-ops-rr.c
@@ -35,13 +35,14 @@
 #include "tcg-accel-ops-rr.h"
 #include "tcg-accel-ops-icount.h"
 
-#if defined(CONFIG_SOFTMMU)  && defined(CONFIG_LATX)
+#if defined(CONFIG_SOFTMMU)
+#if defined(CONFIG_LATX)
 #include "latx-options.h"
 #endif
-
-#if defined(CONFIG_SOFTMMU) && defined(CONFIG_SIGINT)
+#if defined(CONFIG_SIGINT)
 #include "sigint-i386-tcg-la.h"
 #endif
+#endif /* CONFIG_SOFTMMU */
 
 /* Kick all RR vCPUs */
 void rr_kick_vcpu_thread(CPUState *unused)
@@ -50,12 +51,14 @@ void rr_kick_vcpu_thread(CPUState *unused)
 
     CPU_FOREACH(cpu) {
         cpu_exit(cpu);
-#if defined(CONFIG_SOFTMMU)  && defined(CONFIG_LATX)
+#if defined(CONFIG_SOFTMMU)
+#if defined(CONFIG_LATX)
         if (sigint_enabled()) {
-            pthread_kill(cpu->thread->thread, 63);
+            /* pthread_kill(cpu->thread->thread, 63); */
+            latxs_rr_interrupt_self(cpu);
         }
-#endif
-#if defined(CONFIG_SOFTMMU) && defined(CONFIG_SIGINT)
+#endif /* CONFIG_LATX */
+#if defined(CONFIG_SIGINT)
         if (tcgsigint_mode() == TCG_SIGINT_MODE_UNLINK_ONE) {
             pthread_kill(cpu->thread->thread, 63);
         } else if (tcgsigint_mode() == TCG_SIGINT_MODE_UNLINK_ALL) {
@@ -63,7 +66,8 @@ void rr_kick_vcpu_thread(CPUState *unused)
         } else {
             assert(0);
         }
-#endif
+#endif /* CONFIG_SIGINT */
+#endif /* CONFIG_SOFTMMU */
     };
 }
 
@@ -183,11 +187,16 @@ static void *rr_cpu_thread_fn(void *arg)
 
 #ifdef CONFIG_LATX
     latx_lsenv_init(cpu->env_ptr);
+#endif
+
+#if defined(CONFIG_SOFTMMU)
+#if defined(CONFIG_LATX)
     latxs_init_rr_thread_signal(cpu);
 #endif
-#if defined(CONFIG_SOFTMMU) && defined(CONFIG_SIGINT)
+#if defined(CONFIG_SIGINT)
     rr_cpu_tcgsigint_init(cpu);
 #endif
+#endif /* CONFIG_SOFTMMU */
 
     /* wait for initial kick-off after machine start */
     while (first_cpu->stopped) {

accel/tcg/tcg-accel-ops.c
@@ -40,11 +40,10 @@
 
 #if defined(CONFIG_SOFTMMU)
 #include "trace.h"
-#endif
-
-#if defined(CONFIG_SOFTMMU) && defined(CONFIG_SIGINT)
+#if defined(CONFIG_SIGINT)
 #include "sigint-i386-tcg-la.h"
 #endif
+#endif /* CONFIG_SOFTMMU */
 
 /* common functionality among all TCG variants */
 
@@ -104,12 +103,14 @@ void tcg_handle_interrupt(CPUState *cpu, int mask)
         qemu_cpu_kick(cpu);
     } else {
         qatomic_set(&cpu_neg(cpu)->icount_decr.u16.high, -1);
-#if defined(CONFIG_SOFTMMU) && defined(CONFIG_LATX)
+#if defined(CONFIG_SOFTMMU)
+#if defined(CONFIG_LATX)
         if (sigint_enabled()) {
-            pthread_kill(cpu->thread->thread, 63);
+            /* pthread_kill(cpu->thread->thread, 63); */
+            latxs_rr_interrupt_self(cpu);
         }
-#endif
-#if defined(CONFIG_SOFTMMU) && defined(CONFIG_SIGINT)
+#endif /* CONFIG_LATX */
+#if defined(CONFIG_SIGINT)
         if (tcgsigint_mode() == TCG_SIGINT_MODE_UNLINK_ONE) {
             pthread_kill(cpu->thread->thread, 63);
         } else if (tcgsigint_mode() == TCG_SIGINT_MODE_UNLINK_ALL) {
@@ -117,7 +118,8 @@ void tcg_handle_interrupt(CPUState *cpu, int mask)
         } else {
             assert(0);
         }
-#endif
+#endif /* CONFIG_SIGINT */
+#endif /* CONFIG_SOFTMMU */
     }
 }
 

target/i386/latx/optimization/sys-sigint.c
@@ -106,6 +106,34 @@ void latxs_tb_relink(TranslationBlock *utb)
     }
 }
 
+void latxs_rr_interrupt_self(CPUState *cpu)
+{
+    CPUX86State *env = cpu->env_ptr;
+
+    TranslationBlock *ctb = NULL;
+    TranslationBlock *otb = NULL;
+
+    if (env->sigint_flag) {
+        LATX_TRACE(event, "noexectb", 0);
+        return;
+    }
+
+    ctb = env->latxs_int_tb;
+    LATX_TRACE(event, "tbinenv", ctb ? ctb->pc : 0);
+
+    otb = lsenv->sigint_data.tb_unlinked;
+    if (otb == ctb && !ctb) {
+        LATX_TRACE(event, "alreadyunlinkedtb", ctb ? ctb->pc : 0);
+        return;
+    } else {
+        LATX_TRACE(event, "relinkoldtb", otb ? otb->pc : 0);
+        latxs_tb_relink(otb);
+    }
+
+    lsenv->sigint_data.tb_unlinked = ctb;
+    latxs_tb_unlink(ctb);
+}
+
 static int sigint_check_jmp_glue_2_st;
 static int sigint_check_jmp_glue_2_ed;
 

commit 158fdaad627374cb1e692a798650bc6321327490

LATX-SYS: fix latx sigint should send to vcpu signal
caused in commit f515e57339ac899eed53bf070d547331b2c70c43 2021-11-05 16:52

In tcg_handle_interrupt() the function to kick vcpu thread will be called only when current
thread is not the required vcpu thread. So we should send signal to kick the vcpu thread.

---------------------------------------------------------------------------------------

Change-Id: I9970dc3f8501ee6cb85afb9de8335cabb3f08a84
Signed-off-by: NiuGenen <niugen@loongson.cn>

accel/tcg/tcg-accel-ops-rr.c
@@ -54,8 +54,7 @@ void rr_kick_vcpu_thread(CPUState *unused)
 #if defined(CONFIG_SOFTMMU)
 #if defined(CONFIG_LATX)
         if (sigint_enabled()) {
-            /* pthread_kill(cpu->thread->thread, 63); */
-            latxs_rr_interrupt_self(cpu);
+            pthread_kill(cpu->thread->thread, 63);
         }
 #endif /* CONFIG_LATX */
 #if defined(CONFIG_SIGINT)