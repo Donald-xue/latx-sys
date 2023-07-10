ebc796d5629febedec24516aa04353569a972263
NiuGenen <niugen@loongson.cn>
2023-07-10 14:21:05 +0800

    refactor: exit tb save pc before tb link
    Change-Id: Ic746698ac08267caa497ef0084a4f232f23ff0f5
    Signed-off-by: NiuGenen <niugen@loongson.cn>

--- target/i386/latx/translator/sys-eob.c
+++ target/i386/latx/translator/sys-eob.c
@@ -594,6 +594,47 @@ static int branch_is_cross_page(IR1_INST *branch, int n)
     return (next_eip >> TARGET_PAGE_BITS) != (curr_eip >> TARGET_PAGE_BITS);
 }
 
+static void tr_gen_branch_save_next_eip(IR1_INST *branch, int n)
+{
+    int next_eip_size = 32;
+#ifdef TARGET_X86_64
+    next_eip_size = latxs_ir1_addr_size(branch) << 3;
+#endif
+
+    ADDRX next_eip = 0;
+
+    TRANSLATION_DATA *td = lsenv->tr_data;
+    int doit = 0;
+    int ignore_eip = 0;
+
+    if (ir1_is_call(branch) && !ir1_is_indirect_call(branch)) {
+        next_eip = ir1_target_addr(branch);
+        if (latxs_ir1_addr_size(branch) == 2 ||
+            ir1_opnd_size(ir1_get_opnd(branch, 0)) == 16) {
+            next_eip_size = 16;
+        }
+        doit = 1;
+    }
+
+    if (ir1_is_jump(branch) && !ir1_is_indirect_jmp(branch)) {
+        next_eip = ir1_target_addr(branch);
+        next_eip_size = ir1_opnd_size(ir1_get_opnd(branch, 0));
+        ignore_eip = td->ignore_eip_update;
+        doit = 1;
+    }
+
+    if (ir1_is_branch(branch)) {
+        if (n == 0) next_eip = ir1_addr_next(branch);
+        if (n == 1) next_eip = ir1_target_addr(branch);
+        next_eip_size = ir1_opnd_size(ir1_get_opnd(branch, 0));
+        doit = 1;
+    }
+
+    if (doit) {
+        latxs_tr_gen_exit_tb_load_next_eip(ignore_eip, next_eip, next_eip_size);
+    }
+}
+
 /*
  * Details of exit-tb
  *   > EOB: End-Of-TB worker  tr_gen_eob()
@@ -680,75 +721,11 @@ void latxs_tr_generate_exit_tb(IR1_INST *branch, int succ_id)
     }
 
     int already_load_next_eip = 0;
-    switch (opcode) {
-    case X86_INS_CALL:
-        if (!ir1_is_indirect_call(branch)) {
-            next_eip = ir1_target_addr(branch);
-            curr_eip = ir1_addr(branch);
-            if (latxs_ir1_addr_size(branch) == 2 ||
-                ir1_opnd_size(ir1_get_opnd(branch, 0)) == 16) {
-                next_eip_size = 16;
-            }
-            if ((next_eip >> TARGET_PAGE_BITS) != (curr_eip >> TARGET_PAGE_BITS)) {
-                if (option_cross_page_check) {
-                    latxs_tr_gen_exit_tb_load_next_eip(0, next_eip, next_eip_size);
-                    already_load_next_eip = 1;
-                }
-            }
-        }
-        break;
-    case X86_INS_JMP:
-        if (!ir1_is_indirect_jmp(branch)) {
-            next_eip = ir1_target_addr(branch);
-            curr_eip = ir1_addr(branch);
-            next_eip_size = ir1_opnd_size(ir1_get_opnd(branch, 0));
-            if ((next_eip >> TARGET_PAGE_BITS) != (curr_eip >> TARGET_PAGE_BITS)) {
-                if (option_cross_page_check) {
-                    latxs_tr_gen_exit_tb_load_next_eip(td->ignore_eip_update, next_eip, next_eip_size);
-                    already_load_next_eip = 1;
-                }
-            }
-        }
-        break;
-    case X86_INS_JE:
-    case X86_INS_JNE:
-    case X86_INS_JS:
-    case X86_INS_JNS:
-    case X86_INS_JB:
-    case X86_INS_JAE:
-    case X86_INS_JO:
-    case X86_INS_JNO:
-    case X86_INS_JBE:
-    case X86_INS_JA:
-    case X86_INS_JP:
-    case X86_INS_JNP:
-    case X86_INS_JL:
-    case X86_INS_JGE:
-    case X86_INS_JLE:
-    case X86_INS_JG:
-    case X86_INS_JCXZ:
-    case X86_INS_JECXZ:
-    case X86_INS_JRCXZ:
-    case X86_INS_LOOP:
-    case X86_INS_LOOPE:
-    case X86_INS_LOOPNE:
-        next_eip = succ_id ? ir1_target_addr(branch) : ir1_addr_next(branch);
-        next_eip_size = ir1_opnd_size(ir1_get_opnd(branch, 0));
-        curr_eip = ir1_addr(branch);
-        if ((next_eip >> TARGET_PAGE_BITS) != (curr_eip >> TARGET_PAGE_BITS)) {
-            if (option_cross_page_check) {
-                latxs_tr_gen_exit_tb_load_next_eip(0, next_eip, next_eip_size);
-                already_load_next_eip = 1;
-            }
-        }
-        break;
-    default:
-        break;
-    }
-
     if (option_cross_page_check && branch_is_cross_page(branch, succ_id)) {
         tb->next_tb_cross_page[succ_id] = 1;
         need_tb_addr_for_jmp_glue = 1;
+        tr_gen_branch_save_next_eip(branch, succ_id);
+        already_load_next_eip = 1;
     }
 
     if (!need_tb_addr_for_jmp_glue) {
